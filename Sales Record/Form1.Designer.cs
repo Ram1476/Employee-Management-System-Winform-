52233ec35c166695b51bf78a6ecef66ddcb28f1a
Ram1476 <ramilangovan.1299@gmail.com>
2023-02-21 19:05:42 +0530

    Add project files.

---
+++ Sales Record/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Sales_Record
+{
+    partial class salesRec
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.linklblhead = new System.Windows.Forms.LinkLabel();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDes = new System.Windows.Forms.TextBox();
+            this.txtStat = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(27, 145);
+            this.lbl1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(129, 23);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "EmployeeID : ";
+            this.lbl1.Click += new System.EventHandler(this.lblHead_Click);
+            // 
+            // linklblhead
+            // 
+            this.linklblhead.AutoSize = true;
+            this.linklblhead.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linklblhead.LinkColor = System.Drawing.Color.Red;
+            this.linklblhead.Location = new System.Drawing.Point(351, 22);
+            this.linklblhead.Name = "linklblhead";
+            this.linklblhead.Size = new System.Drawing.Size(404, 32);
+            this.linklblhead.TabIndex = 1;
+            this.linklblhead.TabStop = true;
+            this.linklblhead.Text = "Employee Management System";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(27, 209);
+            this.lbl2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(159, 23);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "EmployeeName : ";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(27, 276);
+            this.lbl3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(168, 23);
+            this.lbl3.TabIndex = 3;
+            this.lbl3.Text = "Employee-Salary : ";
+            // 
+            // txtID
+            // 
+            this.txtID.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.Location = new System.Drawing.Point(218, 145);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(104, 29);
+            this.txtID.TabIndex = 4;
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSalary.Location = new System.Drawing.Point(218, 276);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(173, 29);
+            this.txtSalary.TabIndex = 5;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(218, 203);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(173, 29);
+            this.txtName.TabIndex = 6;
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl4.Location = new System.Drawing.Point(566, 151);
+            this.lbl4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(128, 23);
+            this.lbl4.TabIndex = 7;
+            this.lbl4.Text = "Designation : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(566, 209);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 23);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Status : ";
+            // 
+            // txtDes
+            // 
+            this.txtDes.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDes.Location = new System.Drawing.Point(741, 151);
+            this.txtDes.Name = "txtDes";
+            this.txtDes.Size = new System.Drawing.Size(210, 29);
+            this.txtDes.TabIndex = 9;
+            // 
+            // txtStat
+            // 
+            this.txtStat.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStat.Location = new System.Drawing.Point(741, 203);
+            this.txtStat.Name = "txtStat";
+            this.txtStat.Size = new System.Drawing.Size(175, 29);
+            this.txtStat.TabIndex = 10;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(56, 374);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(100, 38);
+            this.btnCreate.TabIndex = 11;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(218, 374);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(108, 38);
+            this.btnRead.TabIndex = 12;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(384, 374);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(107, 38);
+            this.btnUpdate.TabIndex = 13;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(570, 374);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(109, 38);
+            this.btnDelete.TabIndex = 14;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(56, 448);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(733, 250);
+            this.textBox1.TabIndex = 15;
+            // 
+            // salesRec
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(1114, 730);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtStat);
+            this.Controls.Add(this.txtDes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.linklblhead);
+            this.Controls.Add(this.lbl1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "salesRec";
+            this.Text = "Employee Management System";
+            this.Load += new System.EventHandler(this.salesRec_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.LinkLabel linklblhead;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDes;
+        private System.Windows.Forms.TextBox txtStat;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Sales Record/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sales_Record
+{
+    public partial class salesRec : Form
+    {
+        
+        public salesRec()
+        {
+            InitializeComponent();
+        }
+        EmpDet Detail = new EmpDet();
+
+        private void lblHead_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void salesRec_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            // inserting the below mwntioned Values into a Table 
+            //insert into Employees(Emp_Id,Emp_Name,Emp_Salary,Designation,Status)
+            SqlConnection con = new SqlConnection("server = NLTI151\\SQLEXPRESS; database = Empdata; integrated security =  true");
+            int id, salary;
+            string name, des, status;
+            int.TryParse(txtID.Text,out id);
+            int.TryParse(txtSalary.Text, out salary);
+            name = txtName.Text;
+            des = txtDes.Text;
+
+            if (txtStat.Text.ToLower() == "active")
+            {
+                status = "ACTIVE";
+            }
+            else 
+            {
+                status = "IN ACTIVE";
+            }
+
+
+          
+            SqlCommand cmd = new SqlCommand($"insert into Employees(Emp_Id,Emp_Name,Emp_Salary,Designation,Status)\nvalues('{id.ToString()}','{name}','{salary}','{des}','{status}');",con);
+
+            con.Open();
+            try
+            { 
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+            con.Close();
+        }
+    }
+}
